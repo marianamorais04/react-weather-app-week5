fcc6f4638c6c6a68ac3288e85a5bc546372970e3
Mariana Morais <mariana.ds.morais04@gmail.com>
2022-06-24 18:45:44 +0100

    added github

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div> Weather App</div>
+      <div>
+        <h1> Weather App </h1>
+      </div>
+      <div>
+        <p>
+          This project was coded by Mariana Morais{" "}
+          <a
+            href="https://github.com/marianamorais04/react-weather-app-week5"
+            target="_blank"
+          >
+            open-source on GitHub
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
