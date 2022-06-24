f24200634424ca24228c9d72729a3fe344006f0b
Mariana Morais <mariana.ds.morais04@gmail.com>
2022-06-24 19:49:39 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div>
-        <h1> Weather App </h1>
-      </div>
-      <div>
-        <p>
-          This project was coded by Mariana Morais{" "}
-          <a
-            href="https://github.com/marianamorais04/react-weather-app-week5"
-            target="_blank"
-          >
-            open-source on GitHub
-          </a>
-        </p>
+      <div className="container">
+        <div>
+          <h1> Weather App </h1>
+        </div>
+        <div>
+          <p>
+            This project was coded by Mariana Morais{" "}
+            <a
+              href="https://github.com/marianamorais04/react-weather-app-week5"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-source on GitHub
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
